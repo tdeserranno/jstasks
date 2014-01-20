a9b8c30e2c7355ea7a897346528412dfdefdd4e3
cyber02 <cyber02@sat02.CERES.INTRA>
2014-01-20 15:23:53 +0100

    Taak Tag cloud start

---
+++ tagcloud/tagcloud.js
@@ -0,0 +1,14 @@
+//=========GLOBALS==============================================================
+////het array met de tags
+//elk item is zelf een Array met de naam, het huidig aantal stemmen en het vorig aantal stemmen die het kreeg
+var arrTags = [
+  ["Javascript", 1634, 987],
+  ["jQuery", 1111, 34],
+  ["PHP", 1024,1122],
+  ["Asp.Net", 977, 1005],
+  ["Photoshop", 594, 789],
+  ["XML", 40, 666],
+  ["Access", 55, 77],
+  ["Java", 278, 277],
+  ["MySQL", 155, 122]
+];
