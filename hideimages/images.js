6e7856e928cbae26186d78412563ab357624f8f7
cyber02 <cyber02@sat02.CERES.INTRA>
2014-01-24 08:39:17 +0100

    Taak hideimages revisie

--- hideimages/images.js
+++ hideimages/images.js
@@ -1,3 +1,5 @@
+//^^^^IMAGES.JS^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//====GLOBALS===================================================================
 var buttonTexts = {
   en:{
     hide : 'Hide screenshot',
@@ -23,76 +25,82 @@ var lang = 'nl';
 /*
  * add individual toggle option for each screenshot
  */
+//====FUNCTIONS=================================================================
 function addButtons() {
   //get all div.toggle elements
-  var eScreenshotDivs = document.querySelectorAll('article dd div.toggle');
+  var eScreenshotDivs = document.querySelectorAll('div.toggle');
   //add button to each related dd element
   for (var i = 0; i < eScreenshotDivs.length; i++) {
     //create button element
     var eIndivToggleButton = document.createElement('button');
     //set default button text
     eIndivToggleButton.innerHTML = buttonTexts[lang]['hide'];
+    //set button index
+    eIndivToggleButton.setAttribute('data-index', i);
     //set button event handler
-    eIndivToggleButton.setAttribute('onclick', 'hideScreenshot(' + i + ')');
-    //add button to dd before screenshot div
-    eScreenshotDivs[i].parentNode.insertBefore(eIndivToggleButton, eScreenshotDivs[i].parentNode.lastChild.previousSibling);
+    eIndivToggleButton.addEventListener('click', function(event) {
+      toggleScreenshot(this.dataset['index']);
+    });
+    //add button to screenshot div before image
+    eScreenshotDivs[i].insertBefore(eIndivToggleButton, eScreenshotDivs[i].firstChild);
     //bovenstaande kan eenvoudiger !? plaats button voor de image tag??
     //maar dan moet de image verborgen worden en niet de volledige div
   }
 }
-function hideScreenshot(index) {
-  //get all screenshots
-  var eScreenshotDivs = document.querySelectorAll('article dd div.toggle');
+function toggleScreenshot(index) {
+  //get all screenshot divs
+  var eScreenshots = document.querySelectorAll('div.toggle img');
   //get all buttons
-  var eScreenshotButtons = document.querySelectorAll('article dd button');
+  var eScreenshotButtons = document.querySelectorAll('div.toggle button');
   //hide single screenshot based on index
   //check display state, hide when visible or show when hidden
   //change button text accordingly
   //TEST ONVOLLEDIG, moet getest worden of er effectief een inline style is voordat je test of die none is
   //Beter nog is testen op computed style met window.getcomputedstyle of element.currentstyle(IE8 down)
-  var computedStyle = window.getComputedStyle(eScreenshotDivs[index]);
-  if (computedStyle.display === 'none' || eScreenshotDivs[index].style.display === 'none') {
-    eScreenshotDivs[index].style.display = 'block';
+  var imgComputedStyle = window.getComputedStyle(eScreenshots[index]);
+  if (imgComputedStyle.display === 'none' || eScreenshots[index].style.display === 'none') {
+    eScreenshots[index].style.display = 'block';
     eScreenshotButtons[index].innerHTML = buttonTexts[lang]['hide'];
   } else {
-    eScreenshotDivs[index].style.display = 'none';
+    eScreenshots[index].style.display = 'none';
     eScreenshotButtons[index].innerHTML = buttonTexts[lang]['show'];
   }
 }
+//====WINDOW.ONLOAD=============================================================
 window.onload = function() {
   //buttons
   //mainbutton
-  var mainButton = document.getElementById('hoofdknop');
+  var eMainButton = document.getElementById('hoofdknop');
   
   //set main button default button text
-  mainButton.innerHTML = buttonTexts[lang]['hideAll'];
+  eMainButton.innerHTML = buttonTexts[lang]['hideAll'];
   //individual buttons
   addButtons();
   
   //Eventhandlers
   //mainbutton eventhandler
-  mainButton.addEventListener('click',function() {
+  eMainButton.addEventListener('click',function() {
     //get all screenshot buttons
-    var eScreenshotButtons = document.querySelectorAll('article dd button');
-    //get all screenshot divs
-    var eScreenshotDivs = document.querySelectorAll('article dd div.toggle');
+    var eScreenshotButtons = document.querySelectorAll('div.toggle button');
+    //get all screenshots
+    var eScreenshots = document.querySelectorAll('img.screenshot');
     //check hide or show
-    if (mainButton.innerHTML === buttonTexts[lang]['hideAll']) {
+    if (eMainButton.innerHTML === buttonTexts[lang]['hideAll']) {
       //hide screenshots
-      for (var i = 0; i < eScreenshotDivs.length; i++) {
-        eScreenshotDivs[i].style.display = 'none';
+      for (var i = 0; i < eScreenshots.length; i++) {
+        eScreenshots[i].style.display = 'none';
         eScreenshotButtons[i].innerHTML = buttonTexts[lang]['show'];
       }
       //set button text
-      mainButton.innerHTML = buttonTexts[lang]['showAll'];
+      eMainButton.innerHTML = buttonTexts[lang]['showAll'];
     } else {
       //show screenshots
-      for (var i = 0; i < eScreenshotDivs.length; i++) {
-        eScreenshotDivs[i].style.display = 'block';
+      for (var i = 0; i < eScreenshots.length; i++) {
+        eScreenshots[i].style.display = 'block';
         eScreenshotButtons[i].innerHTML = buttonTexts[lang]['hide'];
       }
       //set button text
-      mainButton.innerHTML = buttonTexts[lang]['hideAll'];
+      eMainButton.innerHTML = buttonTexts[lang]['hideAll'];
     }
   });
 };
